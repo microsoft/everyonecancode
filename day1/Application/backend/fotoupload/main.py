f304f3844960ca0eb24f56b83efa44fa9c2fdf78
Martha Splitthoff <masplitt@microsoft.com>
2021-07-13 15:45:05 +0200

    update main.py with foto upload and refactor previous code

--- day1/Application/backend/fotoupload/main.py
+++ day1/Application/backend/fotoupload/main.py
@@ -1,6 +1,6 @@
 import os
 import io
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile
 from azure.storage.blob import BlobServiceClient
 from azure.core.exceptions import ResourceNotFoundError
 from fastapi.responses import StreamingResponse
@@ -17,17 +17,27 @@ container_client = service.get_container_client("images")
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@app.get("/images")
+async def list_images():
     try:
         blobs = [b.name for b in container_client.list_blobs()]
     except ResourceNotFoundError:
         raise HTTPException(status_code=404, detail="Container not found")
-    return {"message": "First Microgram!", "files": blobs}
+    return {"files": blobs}
 
-@app.get("/getImage/{image_name}")
-async def getImage(image_name):
-    blob_container_client = container_client.get_blob_client(image_name)
-    blob = blob_container_client.download_blob()
+@app.get("/images/{image_name}")
+async def images(image_name):
+    try:
+        blob_container_client = container_client.get_blob_client(image_name)
+        blob = blob_container_client.download_blob()
+    except ResourceNotFoundError:
+        raise HTTPException(status_code=404, detail="Your picture was not found, just try to upload again 🤓")
     return StreamingResponse(blob.chunks())
+
+
+@app.post("/upload/")
+async def upload(file: UploadFile = File(...)):
+    container_client.upload_blob(file.filename, file.file, blob_type="BlockBlob", overwrite=True)
+    return {"filename": file.filename}
+
 
