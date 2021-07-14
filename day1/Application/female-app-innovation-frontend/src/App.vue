3f133c38c746de4b400f3e3a5251de6c09ebc62b
Martha Splitthoff <masplitt@microsoft.com>
2021-07-14 12:05:39 +0200

    update frontend and add frontend files

--- day1/Application/female-app-innovation-frontend/src/App.vue
+++ day1/Application/female-app-innovation-frontend/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="./assets/femaletech.jpg" />
+    <ImageList/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import ImageList from "./components/ImageList.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    ImageList
   },
 })
 export default class App extends Vue {}
---
+++ day1/Application/female-app-innovation-frontend/src/components/ImageList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div><h1>Welcome to your first Microgram to upload your pixel-perfect pictures 🤗</h1></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ImageList extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
