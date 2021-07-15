1511692f7654a3cbf0b632dea8a354d4d8c4e1fb
Clemens Wältken <clemens.waltken@microsoft.com>
2021-07-15 12:21:06 +0200

    Load images in frontend

--- day1/Application/female-app-innovation-frontend/src/components/ImageList.vue
+++ day1/Application/female-app-innovation-frontend/src/components/ImageList.vue
@@ -1,17 +1,47 @@
 <template>
-  <div><h1>Welcome to your first Microgram to upload your pixel-perfect pictures 🤗</h1></div>
+  <div>
+    <h1>
+      Welcome to your first Microgram to upload your pixel-perfect pictures 🤗
+    </h1>
+    <div>
+      <div v-for="image in imageList" :key="image">
+        <img width="400px" :src="image.url" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+
+const apiUrl = "https://fotobackendtest.azurewebsites.net";
 
 @Component
 export default class ImageList extends Vue {
+  imageList = [];
 
+  mounted() {
+    this.getImageList();
+  }
+
+  getImageList() {
+    axios
+      .get(`${apiUrl}/images`)
+      .then((response) => {
+        this.imageList = response.data.files.map((file: any) => {
+          return {
+            url: `${apiUrl}/images/${file}`,
+          };
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
