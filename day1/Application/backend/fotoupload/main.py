f4c19ebc9228891b2bb91ff9d5d2493329784c41
Clemens Wältken <clemens.waltken@microsoft.com>
2021-07-15 12:20:48 +0200

    Lint python code

--- day1/Application/backend/fotoupload/main.py
+++ day1/Application/backend/fotoupload/main.py
@@ -9,10 +9,8 @@ from fastapi.responses import StreamingResponse
 connection_string = os.environ['STORAGE_CONNECTIONSTRING']
 service = BlobServiceClient.from_connection_string(conn_str=connection_string)
 
- # Get a client to interact with a specific container - though it may not yet exist
+# Get a client to interact with a specific container - though it may not yet exist
 container_client = service.get_container_client("images")
-
-
 
 app = FastAPI()
 
@@ -25,19 +23,20 @@ async def list_images():
         raise HTTPException(status_code=404, detail="Container not found")
     return {"files": blobs}
 
+
 @app.get("/images/{image_name}")
 async def images(image_name):
     try:
         blob_container_client = container_client.get_blob_client(image_name)
         blob = blob_container_client.download_blob()
     except ResourceNotFoundError:
-        raise HTTPException(status_code=404, detail="Your picture was not found, just try to upload again 🤓")
+        raise HTTPException(
+            status_code=404, detail="Your picture was not found, just try to upload again 🤓")
     return StreamingResponse(blob.chunks())
 
 
 @app.post("/upload/")
 async def upload(file: UploadFile = File(...)):
-    container_client.upload_blob(file.filename, file.file, blob_type="BlockBlob", overwrite=True)
+    container_client.upload_blob(
+        file.filename, file.file, blob_type="BlockBlob", overwrite=True)
     return {"filename": file.filename}
-
-
