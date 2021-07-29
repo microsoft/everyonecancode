188f76f5b677ab32475fa975a571aee143462e3a
Clemens Wältken <clemens.waltken@microsoft.com>
2021-07-29 14:44:12 +0200

    Add vuex-persist

--- day1/Application/female-app-innovation-frontend/src/store/index.ts
+++ day1/Application/female-app-innovation-frontend/src/store/index.ts
@@ -1,16 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {githubUsername:""},
+  state: { githubUsername: "" },
   mutations: {
-    setGithubUsername (state, githubUsername) {
+    setGithubUsername(state, githubUsername) {
       state.githubUsername = githubUsername;
-    }
+    },
   },
   actions: {},
   modules: {},
+  plugins: [vuexLocal.plugin],
 });
-
