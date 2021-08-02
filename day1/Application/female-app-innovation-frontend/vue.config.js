dc1fcba0c65b52ec29f8594af7511c54cd6e88c8
Clemens Wältken <clemens.waltken@microsoft.com>
2021-08-02 14:17:58 +0200

    Add PWA settings to vue.config.js

--- day1/Application/female-app-innovation-frontend/vue.config.js
+++ day1/Application/female-app-innovation-frontend/vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   publicPath: process.env.BASE_URL ? `/${process.env.BASE_URL}` : "/",
+  pwa: {
+    name: "Milligram",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+  },
 };
