66a664cb58f8f5cb8bdc8b22eb2d19dbf2eea95e
Freshchris01 <t-cdiemers@microsoft.com>
2021-08-12 21:11:58 +0200

    Add a rectangle around the face in the FaceAI view

--- day1/Application/female-app-innovation-frontend/src/views/FaceAI.vue
+++ day1/Application/female-app-innovation-frontend/src/views/FaceAI.vue
@@ -1,10 +1,15 @@
 <template>
   <section>
+    <canvas id="ghostVideo" style="display: none"></canvas>
     <div v-if="!faces">
+      <div id="faceRectContainer">
+        <canvas id="faceRect"></canvas>
+      </div>
       <EasyCamera
         v-on:close="onClose"
         ref="camera"
         v-model="picture"
+        :fullscreenZIndex="-1"
         fullscreen
       ></EasyCamera>
     </div>
@@ -46,6 +51,9 @@ export default class FaceAI extends Vue {
 
   picture = "";
   faces: any = null;
+  liveFaces: any = null;
+  faceRect = { width: 92, height: 92, left: 301, top: 149 }; // hardcoded rectangle for testing
+  useLiveFaceDetection = true; // toggle live face detection in camera view
 
   columns = [
     { field: "age", label: "Age" },
@@ -58,6 +66,100 @@ export default class FaceAI extends Vue {
 
   onClose() {
     router.back();
+  }
+
+  created() {
+    window.setInterval(() => {
+      let canvas = document.getElementById("ghostVideo") as HTMLCanvasElement; // declare a canvas element in your html
+      let ctx = canvas.getContext("2d");
+      let video = document.querySelector("video");
+      let imageWidth = 0;
+      let imageHeight = 0;
+
+      // move content of video tag into invisible canvas
+      const v = video;
+      if (ctx == null || v == null) return;
+
+      try {
+        imageWidth = v.videoWidth;
+        imageHeight = v.videoHeight;
+        canvas.width = imageWidth;
+        canvas.height = imageHeight;
+        ctx.fillRect(0, 0, imageWidth, imageHeight);
+        ctx.drawImage(v, 0, 0, imageWidth, imageHeight);
+        const frame: string = canvas.toDataURL();
+        ctx.clearRect(0, 0, imageWidth, imageHeight); // clean the canvas
+
+        // need to use a new canvas here on top of the camera
+        const faceRectCanvas = document.getElementById(
+          "faceRect"
+        ) as HTMLCanvasElement;
+        faceRectCanvas.width = imageWidth;
+        faceRectCanvas.height = imageHeight;
+        var ctx2 = faceRectCanvas.getContext("2d");
+
+        // draw rectangle
+        if (ctx2 != null) {
+          ctx2.rect(
+            this.faceRect.left,
+            this.faceRect.top,
+            this.faceRect.width,
+            this.faceRect.height
+          );
+          ctx2.stroke();
+        }
+
+        if (frame !== "data:," && this.useLiveFaceDetection) {
+          // check if camera is ready
+          fetch(frame)
+            .then((res) => res.blob())
+            .then((blob) => {
+              client.face
+                .detectWithStream(blob, {
+                  returnFaceAttributes: [
+                    "age",
+                    "emotion",
+                    "facialHair",
+                    "smile",
+                    "gender",
+                    "glasses",
+                  ],
+                })
+                .then((response) => {
+                  this.liveFaces = response.map((face) => {
+                    const {
+                      age = 0,
+                      emotion: {
+                        anger = 0,
+                        contempt = 0,
+                        disgust = 0,
+                        fear = 0,
+                        happiness = 0,
+                        neutral = 0,
+                        sadness = 0,
+                        surprise = 0,
+                      } = {},
+                      facialHair: {
+                        moustache = 0,
+                        beard = 0,
+                        sideburns = 0,
+                      } = {},
+                      gender = "",
+                      smile = 0,
+                      glasses = "",
+                    } = { ...face.faceAttributes };
+
+                    console.log(face.faceRectangle);
+
+                    this.faceRect = face.faceRectangle;
+                  });
+                });
+            });
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }, 1000);
   }
 
   @Watch("picture")
@@ -124,4 +226,21 @@ export default class FaceAI extends Vue {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+#faceRectContainer {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  margin-top: -3.25rem;
+}
+
+#faceRect {
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+  z-index: 2;
+}
+</style>
