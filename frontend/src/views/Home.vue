7cd58798abe2b2681178c680db7e24a53329b5a2
Julia Kordick <52427852+jkordick@users.noreply.github.com>
2023-06-09 14:57:31 +0000

    Updated title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,8 +6,8 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: green">
+          Juliagram
         </b-navbar-item>
       </template>
     </b-navbar>
