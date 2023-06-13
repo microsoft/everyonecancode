789be842c508cc253c1293c998f11c8a1329c6fa
Michael McKechney <michael@mckechney.com>
2023-06-13 20:08:48 +0000

    update stuff

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,7 +6,7 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2" style="color: rgb(255, 0, 0)" >
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: rgb(255, 0, 0)"  >
           Sophiagram
         </b-navbar-item>
       </template>
