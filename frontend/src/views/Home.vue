538b1f29b5a63f7a306abcfa30536c46173cf678
Moji <136450927+Mokuj1@users.noreply.github.com>
2023-06-13 14:34:22 +0000

    update title and colour

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,18 +6,17 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color:blue">
+          Mojigram
         </b-navbar-item>
-      </template>
-    </b-navbar>
-    <div class="container">
+      </template
+           >b-navbar
+      <div class="container">
       <Profile />
       <ImageList />
       <Navbar />
     </div>
   </div>
-</template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from "vue-property-decorator";
