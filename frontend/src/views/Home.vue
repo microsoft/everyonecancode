cce1bd52e0c290ba4a2797545e9b8fdde43679c0
katfron <136456515+katfron@users.noreply.github.com>
2023-06-13 14:26:16 +0000

    Test11

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,7 +7,7 @@
         <div id="top"></div>
         <div id="bottom"></div>
         <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+          KatFroGram
         </b-navbar-item>
       </template>
     </b-navbar>
