0252d553e9920e0d552603efc1b66dc09a7939e7
alstrade <136444670+alstrade@users.noreply.github.com>
2023-06-13 13:04:21 +0000

    Update title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,8 +6,8 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color:aqua">
+          Alphagram
         </b-navbar-item>
       </template>
     </b-navbar>
