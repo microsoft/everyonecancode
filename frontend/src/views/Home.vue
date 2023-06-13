637e380884e978cafaadd34281d58699c9ea5a99
Rocky <136450878+Rocky-247@users.noreply.github.com>
2023-06-13 11:03:19 +0000

    Update title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,7 +6,7 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: green>"
           Milligram
         </b-navbar-item>
       </template>
