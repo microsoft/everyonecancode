5cdedc14ad85cab599042fd10ca1a61737033650
Casey Dedekind <130354504+CaseyDed@users.noreply.github.com>
2023-06-14 09:46:53 +0000

    CD-Update-Title-and-Color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,7 +7,7 @@
         <div id="top"></div>
         <div id="bottom"></div>
         <b-navbar-item id="fontnavbar" class="title is-2" style="color: Green">
-          Milligram_CD
+          Caseys_Journey
         </b-navbar-item>
       </template>
     </b-navbar>
