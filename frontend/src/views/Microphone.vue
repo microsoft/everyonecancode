64685c4b83ca1fb708e77dbc0c06e540b5a89679
alejandrorpalacios <136504941+alejandrorpalacios@users.noreply.github.com>
2023-06-14 20:17:50 +0000

    update tile color and language

--- frontend/src/views/Microphone.vue
+++ frontend/src/views/Microphone.vue
@@ -5,6 +5,8 @@
       <select name="lang" @change="onChange($event)" class="custom-select">
         <option value="de-DE" selected>German</option>
         <option value="en-US">English</option>
+        <option value="es-MX">Spanish</option>
+        <option value="hi-IN">Hindi</option>
       </select>
     </div>
     <br />
