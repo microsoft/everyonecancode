89ed1d8be4be135187b1666c9b8f369dd3dfc0f3
alejandrorpalacios <136504941+alejandrorpalacios@users.noreply.github.com>
2023-06-26 17:46:42 +0000

    Update title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,8 +6,8 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2" style="color: green">
-          Sophiagram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: rgb(255, 1, 1)">
+          Alejandro Raziel
         </b-navbar-item>
       </template>
     </b-navbar>
