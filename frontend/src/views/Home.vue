cadc1b4d279eba2db8a1e1995183743b4cae43f7
AC <42763882+aclindkvist@users.noreply.github.com>
2024-05-10 18:25:35 +0000

    Update title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,8 +6,8 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: #224B22">
+          ACgram
         </b-navbar-item>
       </template>
     </b-navbar>
