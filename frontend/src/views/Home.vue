e69197c075c6a6a1451b9aacefc4d4a64b4db157
Alex <41780130+alschroe@users.noreply.github.com>
2024-05-10 12:51:19 +0000

    algram

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,7 +7,7 @@
         <div id="top"></div>
         <div id="bottom"></div>
             <b-navbar-item id="fontnavbar" class="title is-2" style='color: green;'>
-              Kermitgram
+              Alexgram
             </b-navbar-item>
           </template>
         </b-navbar>
