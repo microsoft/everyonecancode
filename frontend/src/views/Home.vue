7a4b916dba855840891eda59d0135cf8353d65fc
Sumire Inoue <110276337+Mire0726@users.noreply.github.com>
2024-05-15 01:35:38 +0000

    changing title

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,7 +7,7 @@
         <div id="top"></div>
         <div id="bottom"></div>
         <b-navbar-item id="fontnavbar" class="title is-2">
-          Sumigram
+          Sumiregram
         </b-navbar-item>
       </template>
     </b-navbar>
