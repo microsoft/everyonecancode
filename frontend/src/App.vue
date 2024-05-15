878300ac5a29f3fe56c9c82549ee3d171d47d9b7
Charis K <harisk@iit.demokritos.gr>
2024-05-15 12:44:40 +0000

    chat bot addition

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,130 +9,24 @@
         <router-view :key="screen" class="router-view-c"></router-view>
       </vue-page-stack>
     </transition>
+    <footer class="footer">
+      Made with love ❤️
+    </footer>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue, Watch } from "vue-property-decorator";
-import ImageList from "./components/ImageList.vue";
-import Profile from "./components/Profile.vue";
-import Navbar from "./components/Navbar.vue";
-
-@Component({
-  components: {
-    Profile,
-    ImageList,
-    Navbar,
-  },
-})
-export default class App extends Vue {
-  transitionName = "forward";
-  screen = "home";
-
-  @Watch("$route")
-  route(to: any, from: any) {
-    if (to.params["stack-key-dir"] === "forward") {
-      this.transitionName = "forward";
-    } else {
-      this.transitionName = "back";
-    }
-  }
-}
-</script>
-
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  position: relative;
-  height: 100%;
+/* Existing styles */
 
-  .cube-btn {
-    border-radius: 5px;
-    padding: 15px;
-    margin: 5px 0;
-  }
-
-  .cube-input {
-    margin: 5px 0;
-    input {
-      padding: 8px;
-    }
-  }
-  .cube-input_active::after {
-    border-radius: 5px;
-  }
-  @keyframes show {
-    0% {
-      transform: translateY(100px);
-    }
-    60% {
-      transform: translateY(-80upx);
-    }
-    100% {
-      transform: translateY(0px);
-    }
-  }
-}
-.router-view-c {
-  position: absolute;
-  transition: opacity 0.5s, transform 0.5s;
-  width: 100%;
-}
-.forward-enter,
-.back-leave-active {
-  opacity: 0.5;
-  transform: translateX(100%);
-}
-.forward-leave-active,
-.back-enter {
-  opacity: 0.5;
-  transform: translateX(-100%);
-}
-@font-face {
-  font-family: HaloHandletter;
-  src: url("../fonts/HaloHandletter.otf") format("opentype");
-}
-#top,
-#bottom,
-#left,
-#right {
-  background: linear-gradient(
-    45deg,
-    #f09433 0%,
-    #e6683c 25%,
-    #dc2743 50%,
-    #cc2366 75%,
-    #bc1888 100%
-  );
+.footer {
   position: fixed;
-}
-#left,
-#right {
-  top: 0;
   bottom: 0;
-  width: 2px;
-}
-#left {
-  left: 0;
-}
-#right {
-  right: 0;
-}
-
-#top,
-#bottom {
   left: 0;
   right: 0;
-  height: 2px;
-}
-#top {
-  top: 0;
-}
-#bottom {
-  bottom: 0;
+  padding: 10px;
+  background-color: #3f0f0f;
+  text-align: center;
+  font-size: 14px;
+  color: #888888;
 }
 </style>
