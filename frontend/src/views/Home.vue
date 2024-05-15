6b61da349282430d67c50b6bf906b100954b75bf
testtsukahara <shiori_tsukahara@gdo.co.jp>
2024-05-15 01:34:15 +0000

    Update title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,11 +6,12 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: red">
+          Milligram 
         </b-navbar-item>
       </template>
     </b-navbar>
+
     <div class="container">
       <Profile />
       <ImageList />
