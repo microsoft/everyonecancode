149ef0c7f3b29a57948ff57fd43f9d350d87fc66
Hal-Okamoto <tete_deau@hotmail.com>
2024-05-15 00:10:45 +0000

    Update title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,7 +6,7 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2" style="color: steelblue">
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: lightcoral">
           Halgram
         </b-navbar-item>
       </template>
